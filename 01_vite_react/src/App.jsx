48fc07d4177b22095bec26125e48ee3572cb0c7c
KrGuddu <krguddu684@gmail.com>
2025-11-23 17:33:23 +0530

    update

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,10 +1,31 @@
+import React, { useEffect, useState } from "react"
+
+function App(){
+  useEffect(()=>{
+    return() =>{
+      
+    }
+  }, [])
 
 
-function App() {
 
-  return (
-    <h1>Chai Aur React With Vite</h1>
-  )
+
+  // const [Count, setCount] = useState(0);
+  // const increment = () => setCount(Count + 1)
+  // const decrement = () => setCount(Count - 1)
+  // const reset = () => setCount(0)
+  
+  // return(
+  //   <div>
+  //     <h1>Count: {Count}</h1>
+
+  //     <button onClick={increment}>Increment</button>
+  //     <button onClick={decrement}>Decrement</button>
+  //     <button onClick={reset}>Reset</button>
+
+      
+  //   </div>
+    
+  // )
 }
-
 export default App
