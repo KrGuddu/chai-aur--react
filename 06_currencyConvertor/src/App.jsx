32173d605880a379152bd4abde4d65c7ea97c544
KrGuddu <krguddu684@gmail.com>
2025-11-28 20:46:31 +0530

    update

--- 06_currencyConvertor/src/App.jsx
+++ 06_currencyConvertor/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1 className=''>New Project</h1>
+    <h1 className='text-white'>Currency Convertor</h1>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
